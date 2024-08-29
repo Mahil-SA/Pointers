5a54881b0bb70d33630e4b30b3776b30517fd308
Mahil Anthony <mahilsamson2016@gmail.com>
2024-08-29 16:16:18 +0530

    Update EXP-10-080.cpp

--- EXP-10-080.cpp
+++ EXP-10-080.cpp
@@ -4,3 +4,51 @@
 //Batch -  ENTC B1
 
 //Program 1 (Swap numbers using call by value)
+#include<iostream>
+using namespace std;
+void swap (int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int a = 7, b = 9;
+    cout<<"Before swap: "<<a<<" "<<b<<endl;
+    swap(a, b);
+    cout<<"After swap: "<<a<<" "<<b<<endl;
+}
+
+//Output
+/*
+Before swap: 7 9
+After swap: 7 9
+/*
+
+//Program 2 (Swap number using call by reference)
+#include<iostream>
+using namespace std;
+void swap (int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int a = 7, b = 9;
+    cout<<"Before swap: "<<a<<" "<<b<<endl;
+    swap(&a, &b);
+    cout<<"After swap: "<<a<<" "<<b<<endl;
+}
+
+//Output
+/*
+Before swap: 7 9
+After swap: 9 7
+*/
